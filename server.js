58d61255b941a4c404a84e21ff91b55decbe4030
Tan Theng Lee <thengleetan@gmail.com>
2015-05-04 18:58:56 +0800

    Route requests using Express router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var router = express.Router();
 
 mongoose.connect('mongodb://localhost:27017/panda-api');
 
@@ -12,60 +13,58 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
+router.route('/')
+	.get(function(req, res){
+		Panda.find(function(err, pandas){
+			if (err) res.send(err);
 
-app.post('/pandas', function(req, res){
-	var panda = new Panda();
-
-	panda.name = req.body.name;
-	panda.description = req.body.description;
-
-	panda.save(function(err){
-		if (err) res.send(err);
-
-		res.send('Panda created!');
-	});
-});
-
-
-app.get('/pandas', function(req, res){
-	Panda.find(function(err, pandas){
-		if (err) res.send(err);
-
-		res.json(pandas);
-	});
-});
-
-
-app.put('/pandas/:panda_name', function(req, res){
-	Panda.findOne({name: req.params.panda_name}, function(err, panda){
-		if (err) res.send (err);
+			res.json(pandas);
+		});
+	})
+	.post(function(req, res){
+		var panda = new Panda();
 
 		panda.name = req.body.name;
+		panda.description = req.body.description;
+
 		panda.save(function(err){
-			if (err) throw err;
-			res.send('Panda updated!');
+			if (err) res.send(err);
+
+			res.send('Panda created!');
 		});
 	});
-});
 
-app.get('/pandas/:panda_name', function(req, res){
-	Panda.findOne({name: req.params.panda_name}, function(err, panda){
-		if (err) res.send(err);
 
-		res.json(panda);
+router.route('/:panda_name')
+	.get(function(req, res){
+		Panda.findOne({name: req.params.panda_name}, function(err, panda){
+			if (err) res.send(err);
+
+			res.json(panda);
+		});
+	})
+	.put(function(req, res){
+		Panda.findOne({name: req.params.panda_name}, function(err, panda){
+			if (err) res.send (err);
+
+			panda.name = req.body.name;
+			panda.save(function(err){
+				if (err) throw err;
+				res.send('Panda updated!');
+			});
+		});
+	})
+	.delete(function(req, res){
+		Panda.remove({name: req.params.panda_name}, function(err, panda){
+			if (err) res.send(err);
+
+			res.send('Panda removed successfully');
+		});
 	});
-});
 
-app.delete('/pandas/:panda_name', function(req, res){
-	Panda.remove({name: req.params.panda_name}, function(err, panda){
-		if (err) res.send(err);
-
-		res.send('Panda removed successfully');
-	});
-});
-
-
+app.use('/pandas', router);
 
 app.listen(8000);
 
 console.log('Listening on port 8000...');
+
